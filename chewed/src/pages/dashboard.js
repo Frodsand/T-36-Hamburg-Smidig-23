1cd1598f2a1e1a82d377a6ad8cc0ff005e946943
Fredrik Rødsand <fredrikrodsand@gmail.com>
2023-05-30 12:12:33 +0200

    created dashboard page

---
+++ chewed/src/pages/dashboard.js
@@ -0,0 +1,10 @@
+function Dashboard(){
+    return(
+        <div>
+            <h1>Dashboard</h1>
+            <p>This is the dashboard page</p>
+        </div>
+    )
+}
+
+export default Dashboard;
