42a68c250e104763de9e3779215c0716f5aab16b
Maria Karlberg <Mariakarlberg@hotmail.com>
2023-05-31 10:24:37 +0200

    created semesterPlansModel schema

---
+++ chewed/backend/models/semesterPlansModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const semesterPlansSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    lecture: {
+        type: [],
+        required: true
+    },
+    user: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model("semesterPlans", semesterPlansSchema)
+
