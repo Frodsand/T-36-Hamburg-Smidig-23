8e0732af6f69f485a5baa62a6fab20b37224944e
OysteinEngell <engell.oystein@gmail.com>
2023-06-01 16:45:52 +0200

    modified recipe controller

--- backend/controllers/recipesControllers.js
+++ backend/controllers/recipesControllers.js
@@ -25,13 +25,51 @@ const getRecipe = async (req, res) => {
     res.status(200).json(recipe);
 }
 
+// const recipesSchema = new Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     description: {
+//         type: String,
+//         required: true
+//     },
+//     instructions: {
+//         type: String,
+//         required: true
+//     },
+//     ingredients: [{
+//         name: {
+//         type: String,
+//         required: true
+//         },
+//         amount: {
+//             type: Number,
+//             required: true
+//         },
+//         unit: {
+//             type: String,
+//             required: true
+//         }
+//     }],
+//     allergies: {
+//         type: [],
+//     },
+//     image: {
+//         type: String,
+//         required: true
+//     }
+// })
+
+//createRecipe(name, description)
+
 // POST and create a new recipe
 const createRecipe = async (req, res) => {
     const {name, description, instructions, ingredients, image} = req.body;
 
     // add to the database
     try{
-        const recipe = await Recipe.create ({name, description, instructions, ingredients,quantity, price, image});
+        const recipe = await Recipe.create ({name, description, instructions, ingredients, name, amount, unit, image});
         res.status(200).json(recipe);
     } catch (error) {
         res.status(400).json({error: error.message});
