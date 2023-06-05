146d2a315ddfcc7b7507161556617cbbf3274a27
elbopero <amanboparai@live.no>
2023-06-05 11:25:15 +0200

    imported login function and applied it to the login route

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,10 +1,11 @@
 const express = require('express')
 
+// importing login function
+const { loginUser } = require('../controllers/loginControllers')
+
 const router = express.Router()
 
 // creating a login route
-router.post('/login', () => {
-    
-})
+router.post('/login', loginUser)
 
 module.exports = router
