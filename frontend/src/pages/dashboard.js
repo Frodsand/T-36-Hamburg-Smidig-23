904a123bfe6b4ef8b6aab0d713602342a990c4ec
Fredrik Rødsand <fredrikrodsand@gmail.com>
2023-06-05 14:24:17 +0200

    simple crd with a go to schedule button

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -2,15 +2,20 @@ import Header from '../components/header';
 import "../styling/dashboard.css"
 
 function Dashboard(){
+
+    const handleOnClick = () => {
+        window.location.href = '/Planner';
+    }
+
     return(
         <>
             <div className='header'>
                 <Header />
             </div>
             <div className='mainGrid'>
-                <div className='welcome'>
-                    <h1 className='welcomeTitle'>Dashboard</h1>
-                    <p className='welcomeMessage'>This is the dashboard page</p>
+                <div className='my-plan-card'>
+                    <h1 className='my-plan-title'>Min årsplan</h1>
+                    <button onClick={handleOnClick} type='button' className='my-plan-button'>Gå til min årsplan</button>
                 </div>
             </div>
         </>
