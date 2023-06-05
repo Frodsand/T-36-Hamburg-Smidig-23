34b54390d410262206b4f3bf5b3562164156e17b
Fredrik Rødsand <fredrikrodsand@gmail.com>
2023-06-05 14:06:59 +0200

    set up grid

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -4,11 +4,13 @@ import "../styling/dashboard.css"
 function Dashboard(){
     return(
         <>
-            <header className='header'><Header /></header>
+            <div className='header'>
+                <Header />
+            </div>
             <div className='mainGrid'>
                 <div className='welcome'>
-                <h1 className='welcomeTitle'>Dashboard</h1>
-                <p className='welcomeMessage'>This is the dashboard page</p>
+                    <h1 className='welcomeTitle'>Dashboard</h1>
+                    <p className='welcomeMessage'>This is the dashboard page</p>
                 </div>
             </div>
         </>
