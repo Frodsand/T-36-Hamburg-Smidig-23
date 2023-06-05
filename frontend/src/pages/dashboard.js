5e3d38a679423fcd03e540aa10d0ffd5db1a7074
Fredrik Rødsand <fredrikrodsand@gmail.com>
2023-06-05 11:38:03 +0200

    added routes to app.js

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -1,6 +1,9 @@
+import Header from '../components/header';
+
 function Dashboard(){
     return(
         <div>
+            <header><Header /></header>
             <h1>Dashboard</h1>
             <p>This is the dashboard page</p>
         </div>
