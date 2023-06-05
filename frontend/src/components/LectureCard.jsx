8e471461164892a42f33c35605ff53d8e744dc06
OysteinEngell <engell.oystein@gmail.com>
2023-06-05 21:17:32 +0200

    Created lecture cards
    added some global styles to fonts

---
+++ frontend/src/components/LectureCard.jsx
@@ -0,0 +1,52 @@
+import './LectureCardStyle.css'
+
+const LectureCard = () => {
+
+    let category = "kategori"
+    let level = "Nivå 1"
+    let lectureTitle = "Undervisningstittel"
+    let recipeTitle = "Oppskrift tittel"
+    let image = "image"
+    
+
+    const handleOnClick = () => {
+        window.location.href = "/Details"
+    }
+
+    return (
+        <article className="card">
+            
+            <div className='top-info'>
+                <p className='category'>{category}</p>
+                <p>{level}</p>
+            </div>
+
+            <h3 className='lectureTitle'>{lectureTitle}</h3>
+            <p className='recipe-info_heading'>Praktisk oppgave:</p>
+            
+            <div className='recipe-info'>    
+                <div className='recipe-info_text'>
+                    <h3 className='recipeTitle'>{recipeTitle}</h3>
+                    <div className='recipe-info_text_allergies'>
+                        <p>Allergier:</p>
+                        <div className='allergies-images'></div>
+                    </div>
+                </div>
+
+                <div className='recipe-info_image'>
+                    {image}
+                </div>
+                
+            </div>
+
+            
+                <button onClick={handleOnClick}>Se undervisningsplan</button>
+            
+            
+            
+            
+        </article>
+    )
+}
+
+export default LectureCard
--- frontend/src/pages/schedule.js
+++ frontend/src/pages/schedule.js
@@ -1,8 +1,55 @@
+import '../styling/schedule.css'
+import LectureCard from '../components/LectureCard'
+
+let semesterPlanTitle = "Undervisningsplan"
+let lecturesArray = [];
+
+const getLectures = () => {
+    //get all lectures in the semesterPlan from db
+}
+
+const printLectureCards = () => {
+    //prints all cards in lectureArray
+
+    //foreach lecture in lecturesArray
+    // -> insert into "gridContainer"
+    // -> 
+}
+
 function Schedule () {
     return (
         <div>
-            <h1>Schedule</h1>
-            <p>This is the schedule page</p>
+            <h1>{semesterPlanTitle}</h1>
+
+            <section className="gridContainer">
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+              <LectureCard/>
+                
+            </section>
+            
+            
         </div>
     )
 }
