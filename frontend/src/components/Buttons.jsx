cdb262da9bdb2209a3c81b608000e54966cfc275
OysteinEngell <engell.oystein@gmail.com>
2023-06-06 13:40:33 +0200

    Button components and stylesheet

---
+++ frontend/src/components/Buttons.jsx
@@ -0,0 +1,24 @@
+
+
+export const PrimaryButton = ({buttonText}) => {
+
+    return <button className="button">{buttonText}</button>
+}
+
+export const outlinePrimaryButton = ({buttonText}) => {
+
+    return <button className="button outline">{buttonText}</button>
+}
+
+export const BigPrimaryButton = ({buttonText}) => {
+
+    return <button className="button big">{buttonText}</button>
+}
+
+
+
+
+
+
+
+
--- frontend/src/pages/schedule.js
+++ frontend/src/pages/schedule.js
@@ -1,8 +1,14 @@
+import PrimaryButton from '../components/Buttons';
+import Buttons from '../components/Buttons'
+
 function Schedule () {
     return (
         <div>
             <h1>Schedule</h1>
             <p>This is the schedule page</p>
+            <div>
+                <PrimaryButton buttonText="Primary button"></PrimaryButton>
+            </div>
         </div>
     )
 }
