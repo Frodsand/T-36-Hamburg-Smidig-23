34528614df9a33ddfe4a334e92645dfde0f7e207
OysteinEngell <engell.oystein@gmail.com>
2023-06-07 11:18:15 +0200

    Removed test buttons from schedule page

--- frontend/src/pages/schedule.js
+++ frontend/src/pages/schedule.js
@@ -7,10 +7,7 @@ function Schedule () {
             <h1>Schedule</h1>
             <p>This is the schedule page</p>
             <div>
-                <PrimaryButton buttonText="Primary button"></PrimaryButton>
-                <OutlinePrimaryButton buttonText="Outline button"></OutlinePrimaryButton>
-                <BigPrimaryButton buttonText="Big button"></BigPrimaryButton>
-                <OutlineBlackButton buttonText="black outline button"></OutlineBlackButton>
+                
             </div>
         </div>
     )
