6c0c5faa1639816b451bb765682987dc86324a69
OysteinEngell <engell.oystein@gmail.com>
2023-06-07 15:38:56 +0200

    Basic jsx structure and styling

--- frontend/src/components/RecipeComponent.jsx
+++ frontend/src/components/RecipeComponent.jsx
@@ -1,25 +1,25 @@
-import "./recipeComponent.css"
+import "../styling/recipeComponent.css"
 
 const RecipeComponent = () => {
     return (
         <section className='recipeContainer'>
             <h2>Oppskrift</h2>
-            <div>image</div>
+            <div className="imageContainer"></div>
 
             <article className='ingredientsContainer' >
                 <h3>Ingredienser</h3>
-                <div className="ingredients">
-                    <h5 className="ingrediensElement">ingrediens</h5>
-                    <h5 className="ingrediensElement">ingrediens</h5>
-                    <h5 className="ingrediensElement">ingrediens</h5>
-                    <h5 className="ingrediensElement">ingrediens</h5>
-                    <h5 className="ingrediensElement">ingrediens</h5>
-                </div>
+                <div className="ingredients"></div>
             </article>
 
             <article className="instructionsContainer">
-                <div className="separator"></div>
+
                 <h3>Fremgangsmåte</h3>
+                <div className="instructions">
+                    
+
+                        
+                    
+                </div>
             </article>
         </section>
     );
