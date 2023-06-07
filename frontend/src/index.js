683ffed00bb8940f900873822b194d6788f1558e
elbopero <amanboparai@live.no>
2023-06-07 15:34:28 +0200

    added context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
-import { IngredientContextProvider } from "./context/ingredientContext.js";
+import { IngredientsContextProvider } from "./context/IngredientContext";
+import { LecturesContextProvider } from "./context/LectureContext";
+import { SemesterPLansContextProvider } from "./context/SemesterPlansContext";
+import { UsersContextProvider } from "./context/UserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      <App />
+    <UsersContextProvider>
+      <SemesterPLansContextProvider>
+        <LecturesContextProvider>
+          <IngredientsContextProvider>
+            <App />
+          </IngredientsContextProvider>
+        </LecturesContextProvider>
+      </SemesterPLansContextProvider>
+    </UsersContextProvider>
   </React.StrictMode>
 );
