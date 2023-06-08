e0659071637870c35ef3d63d80a3ccb5230fb032
Langeland1996 <89384483+Langeland1996@users.noreply.github.com>
2023-06-08 15:39:34 +0200

    refactoring

--- frontend/src/__tests__/dashboard.test.js
+++ frontend/src/__tests__/dashboard.test.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import TestRenderer from 'react-test-renderer';
-import { MemoryRouter } from 'react-router-dom';
 import Dashboard from '../pages/dashboard';
-import { render } from '@testing-library/react';
+import { render, fireEvent  } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 jest.mock('../components/Nav_bar', () => 'MockedNavigationBar');
 jest.mock('../components/header', () => 'MockedHeader');
 jest.mock('../components/ShowLectures', () => 'MockedLectures');
+jest.mock('../components/LectureCard', () => 'MockedLectureCard');
 
-test('Snapshot Dashboard component', () => {
-    const component = TestRenderer.create(
-        <MemoryRouter>
-            <Dashboard />
-        </MemoryRouter>
-    );
-    expect(component.toJSON()).toMatchSnapshot();
+
+test('myFunction should match the snapshot', () => {
+        expect(Dashboard).toMatchSnapshot();
 });
+
+
 test('check heading content', () => {
     const { getByText } = render(<Dashboard />);
     const headingElement = getByText('Min årsplan');
 
     expect(headingElement).toBeInTheDocument();
 });
+
+
