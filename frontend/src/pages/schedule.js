d435bde9b3e2b33e9471baa1adde0d068a8a3754
OysteinEngell <engell.oystein@gmail.com>
2023-06-08 14:28:49 +0200

    prøver på nytt hehe

--- frontend/src/pages/schedule.js
+++ frontend/src/pages/schedule.js
@@ -1,18 +1,35 @@
 import '../styling/schedule.css'
 import LectureCard from '../components/LectureCard'
 import SidebarComponent from '../components/SidebarComponent'
+import { useEffect } from "react";
+import { useLectureContext } from "../hooks/useLectureContext";
 
 
 let semesterPlanTitle = "Undervisningsplan"
 
 function Schedule () {
-    
+    const { lectures, dispatch } = useLectureContext();
+
+  useEffect(() => {
+    const fetchLectures = async () => {
+      const response = await fetch("/api/lectures");
+      const json = await response.json();
+
+      if (response.ok) {
+        dispatch({ type: "SET_LECTURES", payload: json });
+      }
+    };
+
+    fetchLectures();
+  }, [dispatch]);
+
+  console.log('lectures', lectures)
     
     return (
         <div>
             <h1>{semesterPlanTitle}</h1>
             <section className="gridContainer">
-              <LectureCard/>
+            
             </section>
 
             <section className='sidebarContainer'>
