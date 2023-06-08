670f3fc0730ae9afb8fda14b129262c6de358faf
OysteinEngell <engell.oystein@gmail.com>
2023-06-08 13:43:47 +0200

    resolve conflict

--- frontend/src/pages/schedule.js
+++ frontend/src/pages/schedule.js
@@ -2,6 +2,7 @@ import '../styling/schedule.css'
 import LectureCard from '../components/LectureCard'
 import { useEffect } from "react";
 import { useLectureContext } from "../hooks/useLectureContext";
+import SidebarComponent from '../components/SidebarComponent'
 
 
 let semesterPlanTitle = "Undervisningsplan"
@@ -30,6 +31,10 @@ function Schedule () {
             <h1>{semesterPlanTitle}</h1>
             <section className="gridContainer">
               {lectures.map((lecture) => (<LectureCard  lectureObject={lecture} />))}
+            </section>
+
+            <section className='sidebarContainer'>
+                <SidebarComponent></SidebarComponent>
             </section>
         </div>
     )
