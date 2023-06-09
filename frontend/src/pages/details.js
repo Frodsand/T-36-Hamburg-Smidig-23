dc6a772946ec4efd2aa2a628affcd372ca9ebcd9
Fredrik Rødsand <fredrikrodsand@gmail.com>
2023-06-09 10:36:17 +0200

    data is shared between planner and details page

--- frontend/src/pages/details.js
+++ frontend/src/pages/details.js
@@ -1,15 +1,19 @@
 import '../styling/details.css'
 import LecturePlan from'../components/LecturePlan'
+import { useLocation } from 'react-router-dom'
 
 function DetailPage (){
+
+    const location = useLocation();
+    console.log('location', location)
+    const { lectureObject } = location.state;
+
     return (
         <div>
             <h1>Denne ukes plan</h1>
-            
             <div>
-                <LecturePlan/>
+                <LecturePlan lectureObject={lectureObject} />
             </div>
-            
       </div>
     );
 }
--- frontend/src/pages/schedule.js
+++ frontend/src/pages/schedule.js
@@ -4,42 +4,42 @@ import { useEffect } from "react";
 import { useLectureContext } from "../hooks/useLectureContext";
 import SidebarComponent from '../components/SidebarComponent'
 
-
-
 let semesterPlanTitle = "Undervisningsplan"
 
-function Schedule () {
+function Schedule() {
 
-    const { lectures, dispatch } = useLectureContext();
+  const { lectures, dispatch } = useLectureContext();
 
-    useEffect(() => {
-        const fetchLectures = async () => {
-          const response = await fetch("/api/lectures");
-          const json = await response.json();
-    
-          if (response.ok) {
-            dispatch({ type: "SET_LECTURES", payload: json });
-          }
-        };
-    
-        fetchLectures();
-      }, [dispatch]);
-    
-      console.log('lectures', lectures)
-    
-    
-    return (
-        <div>
-            <h1>{semesterPlanTitle}</h1>
-            <section className="gridContainer">
-              {lectures && lectures.map((lecture) => (<LectureCard  lectureObject={lecture} />))}
-            </section>
+  useEffect(() => {
+    const fetchLectures = async () => {
+      const response = await fetch("/api/lectures");
+      const json = await response.json();
 
-            <section className='sidebarContainer'>
-                <SidebarComponent></SidebarComponent>
-            </section>
-        </div>
-    )
+      if (response.ok) {
+        dispatch({ type: "SET_LECTURES", payload: json });
+      }
+    };
+
+    fetchLectures();
+  }, [dispatch]);
+
+  console.log('lectures', lectures)
+
+
+  return (
+    <div>
+      <h1>{semesterPlanTitle}</h1>
+      <section className="gridContainer">
+        {lectures && lectures.map((lecture) => (
+          <LectureCard key={lecture._id} lectureObject={lecture} />
+        ))}
+      </section>
+
+      <section className='sidebarContainer'>
+        <SidebarComponent />
+      </section>
+    </div>
+  )
 }
 
 export default Schedule;
