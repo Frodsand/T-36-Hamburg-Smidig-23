15ea066589f4e9b3d515b8867500445c87b2b383
elbopero <amanboparai@live.no>
2023-06-11 01:04:08 +0200

    created a button for logout and importing logout

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,14 +1,21 @@
 import "../styling/header.css"
 import { AiOutlineUser } from "react-icons/ai"
 import { useAuthContext } from "../hooks/useAuthContext"
+import { useLogout } from '../hooks/useLogout'
 
 const Header = () => {
 
     const { user } = useAuthContext()
+    const { logout } = useLogout()
     const logo = require("../resources/images/logo.png")
+
 
     const handleOnClick = () => {
         window.location.href = "/home"
+    }
+
+    const handleClick = () => {
+        logout()
     }
 
     return (
@@ -21,6 +28,9 @@ const Header = () => {
             <span>Logged in user: { user.username }</span>
             </div>
             )}
+            <div>
+                <button onClick={handleClick}>Logout</button>
+            </div>
             <div className="user-icon-box">
                 <AiOutlineUser className="user-icon"/>
             </div>
