f97b46ea68532977630ac65fb9e4ac02835a275e
Maria Karlberg <Mariakarlberg@hotmail.com>
2023-06-12 11:24:16 +0200

    added recipe data

--- backend/scripts/lecturesScript.js
+++ backend/scripts/lecturesScript.js
@@ -19,7 +19,7 @@ async function createLectureDocuments(){
                 level: "1",
                 title: "Introduksjon til sunn matlaging",
                 lectureImage: "gryndleggendeImage.jpg",
-                learningOutcome: "I denne leksjonen vil du lære hvorfor det er viktig å spise sunn mat. Vi vil utforske hvordan sunn mat gir næring til kroppen din, holder deg frisk og energisk, og hjelper deg med å vokse og utvikle deg på best mulig måte.",
+                learningOutcome: "I denne undervisningen skal elevene lære om Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet vestibulum diam, eget bibendum nisl. Pellentesque elit ex, semper eu nisl id, rutrum faucibus neque. Phasellus nec consequat sapien, interdum pretium enim.v  kjsbdksbchjdsbcljhvsdjvsghdcvaslhxvasuvduwqvduoqwvdguqvdgvscgvdogufveoufdvepqwuydveuvceuwvfcuevcupewvcpebvpiyasvd",
                 recipeTitle: "Fruktsalat",
                 recipeImage: "frukt_salat.jpg",
                 instructions: "1. Vask og skjær ulike frukter i biter. 2. Bland alle fruktene i en bolle. 3. Server og nyt!",
@@ -46,8 +46,65 @@ async function createLectureDocuments(){
                 recipeImage: "mini_pizza.jpg",
                 instructions: "1. Lag fullkornsbunn ved å blande mel, gjær, olivenolje, salt og vann. La deigen heve. 2. Kjevle ut deigen og form små pizzaer. 3. Tilsett ønsket saus og fyll. 4. Stek i ovnen til osten er smeltet og bunnen er sprø. 5. Server og nyt!",
                 ingredients: ["Hvetemel", "Gjær", "Olivenolje", "Salt", "Vann", "Tomatsaus", "Revet ostst", "Skinke", "Paprika", "Mais", "Sopp"]
-            }
-        ];
+            },
+            
+                {
+                "category": "Matvaregrupper",
+                "level": "2",
+                "title": "Meieriprodukter",
+                "lectureImage": "matvaregrupperImage.png",
+                "learningOutcome": "I denne leksjonen vil du bli kjent med forskjellige meieriprodukter. Vi skal lære om de sunne fordelene med å inkludere melk, yoghurt og ost i kostholdet vårt. Du vil også oppdage hvorfor kalsium er viktig for å bygge sterke ben og tenner.",
+                "recipeTitle": "Fruktig Yoghurtmiks",
+                "recipeImage": "YoghurtMix.jpg",
+                "instructions": "1. Finn en skål eller en kopp. 2. Hell litt yoghurt i bunnen av beholderen. 3. Kutt opp ulike frukter som epler, bananer, og druer. 4. Legg fruktbitene over yoghurten. 5. Gjenta lagene med yoghurt og frukt til beholderen er full. 6. Topp det hele med litt honning eller granola for ekstra smak. 7. Nyt din fruktige yoghurtmiks!",
+                "ingredients": ["Yoghurt", "Epler", "Bananer", "Druer", "Honning", "Granola"]
+                },
+
+                {
+                "category": "Ernæring",
+                 "level": "2",
+                 "title": "Kornprodukter",
+                 "lectureImage": "matvaregrupperImage.png",
+                 "learningOutcome": "I denne leksjonen vil du bli kjent med ulike kornprodukter og hvorfor de er en viktig del av et balansert kosthold. Vi skal utforske forskjellige typer korn som havre, ris og brød. Du vil lære om hvordan kornprodukter gir kroppen vår energi og fiber for å holde oss mette og sunne.",
+                 "recipeTitle": "Fargerikt Frokostblanding",
+                 "recipeImage": "Frokostblanding.jpg",
+                 "instructions": "1. Finn en bolle eller et glass. 2. Tilsett en blanding av ulike frokostblandinger som havregryn, cornflakes og puffet ris. 3. Kutt opp fargerike frukter som jordbær, blåbær og bananer. 4. Dryss fruktbitene over frokostblandingen. 5. Hell melk eller yoghurt over blandingen. 6. Rør godt og nyt din fargerike frokostblanding!",
+                 "ingredients": ["Havregryn", "Cornflakes", "Puffet ris", "Jordbær", "Blåbær", "Bananer", "Melk", "Yoghurt"]
+                },
+                {
+                "category": "Matvaregrupper",
+                "level": "2",
+                "title": "Proteinkilder",
+                "lectureImage": "matvaregrupperImage.png",
+                "learningOutcome": "I denne leksjonen vil du lære om forskjellige proteinkilder og hvorfor de er viktige for kroppen vår. Vi skal utforske matvarer som egg, bønner, fisk og kjøtt. Du vil oppdage hvorfor proteiner er nødvendige for å bygge og reparere celler, samt opprettholde en sunn vekst og utvikling.",
+                "recipeTitle": "Egg & Grønnsakssmørbrød",
+                "recipeImage": "EggSmorbrod.jpg",
+                "instructions": "1. Kok et egg i ca. 8-10 minutter til det er hardkokt. 2. Skrell og skjær egget i skiver. 3. Smør et lag av ditt favorittgrønnsakspålegg på en brødskive. 4. Legg eggskivene over pålegget. 5. Legg et lag av ferske grønnsaker som agurk, tomat og salatblader over egget. 6. Legg en annen brødskive på toppen. 7. Skjær smørbrødet i to og nyt ditt næringsrike egg- og grønnsakssmørbrød!",
+                "ingredients": ["Egg", "Grønnsakspålegg", "Brød", "Agurk", "Tomat", "Salatblader"]
+                },
+                {
+                "category": "Matvaregrupper",
+                "level": "3",
+                "title": "Sunn Snack: Smoothie Bowl",
+                "lectureImage": "matvaregrupperImage.png",
+                "learningOutcome": "I denne avanserte leksjonen vil du lære å lage en næringsrik smoothie bowl. Vi vil utforske ulike ingredienser som frukt, grønnsaker og supermat for å lage en deilig og sunn snack. Du vil oppdage hvordan smoothie bowls kan være en flott måte å få i seg viktige næringsstoffer på en morsom og kreativ måte.",
+                "recipeTitle": "Supergrønn Smoothie Bowl",
+                "recipeImage": "SmoothieBowl.jpg",
+                "instructions": "1. I en blender, tilsett en håndfull frisk spinat, en moden banan, en avokado og en kopp frossen mango. 2. Tilsett en skje med spirulina pulver for ekstra næringsstoffer. 3. Hell i litt mandelmelk eller annen plantebasert melk for å hjelpe blandingen å blande seg godt. 4. Blend alt sammen til en jevn og tykk konsistens. 5. Hell smoothieblandingen i en bolle. 6. Topp med skivede bær, kokosflak, hakkede nøtter og en teskje chiafrø. 7. Nyt din supergrønne smoothie bowl!",
+                "ingredients": ["Spinat", "Banan", "Avokado", "Mango", "Spirulina pulver", "Mandelmelk", "Bær", "Kokosflak", "Nøtter", "Chiafrø"]
+                },
+                {
+                "category": "Matvaregrupper",
+                "level": "2",
+                "title": "Fiskeprodukter",
+                "lectureImage": "matvaregrupperImage.png",
+                "learningOutcome": "I denne leksjonen vil du bli kjent med forskjellige fiskeprodukter og hvorfor de er en viktig del av et sunt kosthold. Vi vil utforske ulike typer fisk som laks, tunfisk og torsk, og lære om fordelene med å inkludere fisk i kostholdet vårt. Du vil også oppdage hvordan fisk er en god kilde til omega-3 fettsyrer som bidrar til en sunn hjernefunksjon og hjerte-helse.",
+                "recipeTitle": "Grillet Fisketaco",
+                "recipeImage": "GrilletFisketaco.jpg",
+                "instructions": "1. Forvarm grillen til middels varme. 2. Pensle fisken (laks, tunfisk eller torsk) med litt olje og krydre den med salt, pepper og favorittkrydderet ditt. 3. Grill fisken i ca. 4-6 minutter på hver side, eller til den er gjennomstekt og flaker seg lett med en gaffel. 4. Varm tortillaene på grillen i noen sekunder på hver side. 5. Legg den grillet fiskefileten i hver tortilla. 6. Topp med avokadokrem, finhakket løk, fersk koriander og litt limejuice. 7. Brett tortillaene sammen og nyt dine deilige grillet fisketacoer!",
+                "ingredients": ["Fisk (laks, tunfisk eller torsk)", "Olje", "Salt", "Pepper", "Krydder", "Tortilla", "Avokadokrem", "Løk", "Fersk koriander", "Limejuice"]
+                }
+                ];
 
         const collectionCount = await lecturesModel.countDocuments()
 
